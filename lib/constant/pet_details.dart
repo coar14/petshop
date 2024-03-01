ca3b839b8389b5122d8b68427c47beb4a6ae11ba
cor <corchxmasong@gmail.com>
2024-03-01 20:19:24 +0800

    changes

--- lib/constant/pet_details.dart
+++ lib/constant/pet_details.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:petshop/constant/bottom_navbar.dart';
 import 'package:petshop/constant/list.dart';
 import 'package:petshop/providers/cart_provider.dart';
 import 'package:provider/provider.dart';
